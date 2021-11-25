a790c09ec19a926fd8930c9d896f655713a388ce
Chaitra <chaitra.srinivas@gmail.com>
2021-11-25 17:00:34 +1100

    Comment model + seeds

--- public/js/login.js
+++ public/js/login.js
@@ -1,60 +1,53 @@
 const loginFormHandler = async (event) => {
-    event.preventDefault();
+  event.preventDefault();
 
-    // User enterd values coming from the login form
+  // User enterd values coming from the login form
 
-    const email = document.querySelector('#email-login').value.trim();
-    const password = document.querySelector('#password-login').value.trim();
+  const email = document.querySelector("#email-login").value.trim();
+  const password = document.querySelector("#password-login").value.trim();
 
-    if (email && password) {
-        const response = await fetch('/api/users/login', {
-            method: 'POST',
-            body: JSON.stringify({ email, password }),
-            headers: { 'Content-Type': 'application/json' }
-        });
+  if (email && password) {
+    const response = await fetch("/api/users/login", {
+      method: "POST",
+      body: JSON.stringify({ email, password }),
+      headers: { "Content-Type": "application/json" },
+    });
 
-        if (response.ok) {
-           document.location.replace('/profile');
-           console.log("Here!");
-        } else {
-            console.log("Bad here!");
-            alert(response.statusText);
-        }
+    if (response.ok) {
+      document.location.replace("/profile");
+    } else {
+      console.log("Bad here!");
     }
-
-}
-
+  }
+};
 
 const signUpFormHandler = async (event) => {
-    event.preventDefault();
+  event.preventDefault();
 
-    // User enterd values coming from the login form
+  // User enterd values coming from the login form
 
-    const name = document.querySelector('#name-signup').value.trim();
-    const email = document.querySelector('#email-signup').value.trim();
-    const password = document.querySelector('#password-signup').value.trim();
+  const name = document.querySelector("#name-signup").value.trim();
+  const email = document.querySelector("#email-signup").value.trim();
+  const password = document.querySelector("#password-signup").value.trim();
 
-    if (name && email && password) {
+  if (name && email && password) {
+    const response = await fetch("", {
+      method: "POST",
+      body: JSON.stringify({ name, email, body }),
+      headers: { "Content-Type": "application/json" },
+    });
 
-        const response = await fetch('', {
-            method: 'POST',
-            body: JSON.stringify({ name, email, body }),
-            headers: { 'Content-Type': 'application/json' },
-
-        });
-
-        if (response.ok) {
-            document.location.replace('/profile');
-        } else {
-            alert(response.statusText);
-        }
+    if (response.ok) {
+      document.location.replace("/profile");
+    } else {
+      alert(response.statusText);
     }
-
+  }
 };
 
 document
-    .querySelector('.login-form')
-    .addEventListener('submit', loginFormHandler);
+  .querySelector(".login-form")
+  .addEventListener("submit", loginFormHandler);
 document
-    .querySelector('.signup-form')
-    .addEventListener('submit', signUpFormHandler);
+  .querySelector(".signup-form")
+  .addEventListener("submit", signUpFormHandler);
