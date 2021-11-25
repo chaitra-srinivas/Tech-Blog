122f049da1aef26a587429c5e8e8e408b0349889
Chaitra <chaitra.srinivas@gmail.com>
2021-11-25 17:26:01 +1100

    home routes for all blogs

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,18 +1,46 @@
-const router = require('express').Router();
-const {User} = require('../models');
-const withAuth = require('../utils/auth');
+const router = require("express").Router();
+const { User, Blog, Comment } = require("../models");
+const withAuth = require("../utils/auth");
 
-router.get('/', async (req,res)=>{
+router.get("/", async (req, res) => {
+  try {
+    const blogData = await Blog.findAll({
+      include: [
+        {
+          model: Comment,
+          attributes: ["id", "comment_content", "user_id", "blog_id"],
+          include: { model: User, attributes: "name" },
+        },
+        {
+          model: User,
+          attributes: ["name"],
+        },
+      ],
+    });
 
+    // serialize data so the template can read it
+    const blogs = blogData.map((blog) => {
+      blog.get({ plain: true });
+    });
+
+    // pass serialized data and session flag into template
+
+    res.render("homepage", {
+      blogs,
+      logged_in: true,
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
-router.get('/login', (req,res)=>{
-    if(req.session.logged_in){
-        res.redirect('/profile');
-        return;
-    }else{
-        res.render('login');
-    }
+router.get("/login", (req, res) => {
+  if (req.session.logged_in) {
+    res.redirect("/profile");
+    return;
+  } else {
+    res.render("login");
+  }
 });
 
 module.exports = router;
